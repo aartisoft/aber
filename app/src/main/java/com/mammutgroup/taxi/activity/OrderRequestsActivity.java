bc9c65d268ef7de812ce83d9993cd7b216e3cb4f
mushtu <mojtabayazdani88@gmail.com>
2016-06-09 01:01:03 +0430

    add fab to driver home, handles order selection

--- app/src/main/java/com/mammutgroup/taxi/activity/OrderRequestsActivity.java
+++ app/src/main/java/com/mammutgroup/taxi/activity/OrderRequestsActivity.java
@@ -1,7 +1,8 @@
 package com.mammutgroup.taxi.activity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.afollestad.materialcab.MaterialCab;
 import com.mammutgroup.taxi.service.remote.rest.api.order.model.Order;
 
@@ -18,45 +21,80 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class OrderRequestsActivity extends AppCompatActivity implements OrdersRecyclerAdapter.Callback, MaterialCab.Callback{
+public class OrderRequestsActivity extends AppCompatActivity implements OrdersRecyclerAdapter.Callback, MaterialCab.Callback {
 
     List<Order> orders;
     MaterialCab mCab;
     OrdersRecyclerAdapter orderAdapter;
+    @Bind(R.id.list)
     RecyclerView recyclerView;
+    @Bind(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_requests);
+        ButterKnife.bind(this);
         setupOrders();
+        setupToolbar();
         setupLayout(savedInstanceState);
     }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_order_requests, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_order_requests, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+
     }
 
     private void setupLayout(Bundle savedInstanceState) {
-        recyclerView = (RecyclerView) findViewById(R.id.list);
-
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        orderAdapter = new OrdersRecyclerAdapter(orders,this);
+        orderAdapter = new OrdersRecyclerAdapter(orders, this);
         recyclerView.setAdapter(orderAdapter);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
         if (savedInstanceState != null) {
             mCab = MaterialCab.restoreState(savedInstanceState, this, this);
             orderAdapter.restoreState(savedInstanceState);
         }
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                syncRequests();
+            }
+        });
 
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void setupOrders() {
         orders = new ArrayList<Order>();
-        for(int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             Order order = new Order();
             order.setSourceAddress(UUID.randomUUID().toString());
             order.setDestinationAddress(UUID.randomUUID().toString());
@@ -73,6 +111,12 @@ public class OrderRequestsActivity extends AppCompatActivity implements OrdersRe
             super.onBackPressed();
         }
     }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        onBackPressed();
+//        return true;
+//    }
 
     @Override
     public boolean onCabCreated(MaterialCab materialCab, Menu menu) {
@@ -92,18 +136,17 @@ public class OrderRequestsActivity extends AppCompatActivity implements OrdersRe
 
     @Override
     public boolean onCabItemClicked(MenuItem menuItem) {
-        switch (menuItem.getItemId())
-        {
+        switch (menuItem.getItemId()) {
             case R.id.mit_take_passenger:
                 //showTakePassengerConfirmationDialog();
                 sendTripAcceptedByDriver();
                 break;
-            case R.id.mit_remove:
-                //showItemRemovalConfirmationDialog();
-                removeTripItem();
-                break;
         }
         return true;
+    }
+
+    private void syncRequests() {
+        //TODO call server for sync
     }
 
 //
@@ -151,14 +194,25 @@ public class OrderRequestsActivity extends AppCompatActivity implements OrdersRe
 //
 //    }
 
-    private void sendTripAcceptedByDriver()
-    {
+    private void sendTripAcceptedByDriver() {
 
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(R.string.message_progress_working);
+        final AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                alert.dismiss();
+
+            }
+        }, 5000);
+
+        //TODO
 
     }
 
-    private void removeTripItem()
-    {
+    private void removeTripItem() {
 
     }
 
