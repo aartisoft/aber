9b2d7d13ba517056804d57e8077a46205a4bd47f
mushtu <mojtabayazdani88@gmail.com>
2016-06-08 23:20:13 +0430

    change login and verification code layout, add timer for code expiration

--- app/src/main/java/com/mammutgroup/taxi/model/Driver.java
+++ app/src/main/java/com/mammutgroup/taxi/model/Driver.java
@@ -1,23 +1,19 @@
 package com.mammutgroup.taxi.model;
 
 
+import com.mammutgroup.taxi.service.remote.rest.api.order.model.Order;
+import com.mammutgroup.taxi.service.remote.rest.api.user.model.User;
+
 /**
  * @author mushtu
  * @since 5/31/16.
  */
-public class Driver {
+public class Driver extends User {
 
     private BasicLocation location;
     private volatile boolean readyForService;
-
-
-    public synchronized BasicLocation getLocation() {
-        return location;
-    }
-
-    public synchronized void setLocation(BasicLocation location) {
-        this.location = location;
-    }
+    private DriverStatus status;
+    private Order currentOrder;
 
     public boolean isReadyForService() {
         return readyForService;
@@ -26,4 +22,36 @@ public class Driver {
     public void setReadyForService(boolean readyForService) {
         this.readyForService = readyForService;
     }
+
+    public Order getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public void setCurrentOrder(Order currentOrder) {
+        this.currentOrder = currentOrder;
+    }
+
+    public DriverStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DriverStatus status) {
+        this.status = status;
+    }
+
+    public synchronized BasicLocation getLocation() {
+        return location;
+    }
+
+    public synchronized void setLocation(BasicLocation location) {
+        this.location = location;
+    }
+//
+//    public boolean isReadyForService() {
+//        return readyForService;
+//    }
+//
+//    public void setReadyForService(boolean readyForService) {
+//        this.readyForService = readyForService;
+//    }
 }
---
+++ app/src/main/java/com/mammutgroup/taxi/model/DriverStatus.java
@@ -0,0 +1,14 @@
+package com.mammutgroup.taxi.model;
+
+/**
+ * @author mushtu
+ * @since 6/7/16.
+ */
+public enum DriverStatus {
+
+    OUT_OF_SERVICE,
+    READY_FOR_SERVICE,
+    ON_THE_ROAD_TO_PASSENGER,
+    SERVICING_PASSENGER
+
+}
