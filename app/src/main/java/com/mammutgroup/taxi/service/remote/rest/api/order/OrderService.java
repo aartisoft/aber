08313a1fc1f7398970175eed3c5b2866fc62bbd8
iraj <aliamiri68@gmail.com>
2016-06-09 15:38:23 +0430

    add bottom layout to map , handle its fields and add price service

--- app/src/main/java/com/mammutgroup/taxi/service/remote/rest/api/order/OrderService.java
+++ app/src/main/java/com/mammutgroup/taxi/service/remote/rest/api/order/OrderService.java
@@ -1,6 +1,8 @@
 package com.mammutgroup.taxi.service.remote.rest.api.order;
 
 import com.mammutgroup.taxi.service.remote.rest.api.order.model.Order;
+import com.mammutgroup.taxi.service.remote.rest.api.order.model.PriceResponse;
+
 import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.POST;
@@ -12,5 +14,8 @@ import retrofit.http.POST;
 public interface OrderService {
 
     @POST("/order")
-    void postOrder(@Body Order order, Callback<Order> callback);
+    void createOrder(@Body Order order, Callback<Order> callback);
+
+    @POST("/order/price")
+    void calculatePrice(@Body Order order, Callback<PriceResponse> callback);
 }
